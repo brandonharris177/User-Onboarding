d87cad0c6c8b89b4f5a47b6e06851501095af25b
Brandon-Harris <brandonharris177@gmail.com>
2019-08-21 16:50:52 -0600

    formik-setup/export

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,8 +1,18 @@
-const Form = () => {
+import React from "react";
+import { Form, Feild, withFormik } from "formik";
+// import axios from "axios";
+
+const UserForm = () => {
+
     return (
         <>
         </>
-    )
-}
+    );
+};
 
-export default Form
+// const FormikHOC = withFormik({}); //this just renames the withFormic function
+// const UserFormWithFormik = FormikHOC({})(UserForm); //and now we apply it to the thing we want to export
+// export default UserFormWithFormik;
+//this process is consolidated into the following line:
+
+export default withFormik({})(UserForm);
